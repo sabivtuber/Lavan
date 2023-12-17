fe27113167ea692d7249dbacdc917f53a625a9f0
Sabi <sabivtuber@proton.me>
2023-12-17 08:11:17 -0300

    Repeater Parse Adapter

--- src/parser/adapters/repeat.rs
+++ src/parser/adapters/repeat.rs
@@ -1,6 +1,7 @@
-use crate::{parse_stream_oncer::traits::ParserOnce, stream::traits::Stream};
-
-use super::*;
+use crate::data::prelude::*;
+use crate::parser::prelude::*;
+use crate::stream::traits::Stream;
+use std::marker::PhantomData;
 
 #[derive(Debug, Default)]
 pub enum OneOrMul<T, Col = Vec<T>>
@@ -67,6 +68,8 @@ where
 }
 
 pub mod mode {
+    use std::marker::PhantomData;
+
     #[derive(Clone, Copy, Debug)]
     pub struct UntilErr;
 
@@ -99,212 +102,209 @@ pub type RepeatMax<Par, Col = ()> = Repeater<Par, Maximum, Col>;
 pub type RepeatExact<Par, Col = ()> = Repeater<Par, Exact, Col>;
 
 #[derive(Clone, Copy, Debug)]
-pub struct Repeater<Par, Mod = UntilErr, Col = ()> {
-    pub(in super::super) parse_stream_oncer: Par,
-    pub(in super::super) mode: Mod,
-    pub(in super::super) collector: Col,
+pub struct Repeater<Par, Mod, Col = ()> {
+    parser: Par,
+    mode: Mod,
+    collector: Col,
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, UntilErr, Col>
-where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
-{
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Infallible;
-
-    #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let mut collector = std::mem::take(&mut self.collector);
-        while let Ok(value) = self
-            .parse_stream_oncer
-            .as_ref()
-            .non_terminal()
-            .parse_stream_once(input)
-        {
-            collector.push(value);
+impl<Par, Mod> Repeater<Par, Mod> {
+    pub(crate) fn new<Str>(parser: Par, mode: Mod) -> Self
+    where
+        Str: Stream,
+        Par: ParserMut<Str>,
+        Par::Output: ResultConvertable,
+    {
+        Self {
+            parser,
+            mode,
+            collector: (),
         }
-        Ok(collector)
     }
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, UntilEOI, Col>
+impl<Str, Par, Col, Out> ParserOnce<Str> for Repeat<Par, Col>
 where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: Recoverable + UnerringConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Out::Infallible;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let mut collector = std::mem::take(&mut self.collector);
-
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
         loop {
-            match input.peek_next() {
+            match (&mut self.parser)
+                .non_terminal()
+                .parse_stream_mut(input)
+                .into_result()
+            {
+                Ok(val) => self.collector.extend([val]),
+                Err(_) => return <Out::Infallible as Pure>::pure(self.collector),
+            }
+        }
+    }
+}
+
+impl<Str, Par, Col, Out> ParserOnce<Str> for RepeatEOI<Par, Col>
+where
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: ResultConvertable<Value = Col>,
+{
+    type Output = Out;
+
+    #[inline]
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
+        loop {
+            match input.peek() {
                 Some(_) => {}
-                None => return Ok(collector),
+                None => return Out::ok(self.collector),
             }
 
-            collector.push(self.parse_stream_oncer.as_ref().parse_stream_once(input)?);
+            self.collector
+                .extend([result!(self.parser.parse_stream_mut(input))]);
         }
     }
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, Maximum, Col>
+impl<Str, Par, Col, Out> ParserOnce<Str> for RepeatMax<Par, Col>
 where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: Recoverable + UnerringConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Infallible;
+    type Output = Out::Infallible;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let mut collector = std::mem::take(&mut self.collector);
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
         for _ in 0..self.mode.0 {
-            match self
-                .parse_stream_oncer
-                .as_ref()
+            match (&mut self.parser)
                 .non_terminal()
-                .parse_stream_once(input)
+                .parse_stream_mut(input)
+                .into_result()
             {
-                Ok(value) => collector.push(value),
+                Ok(val) => self.collector.extend([val]),
                 Err(_) => break,
             }
         }
-        Ok(collector)
+        <Out::Infallible as Pure>::pure(self.collector)
     }
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, Exact, Col>
+impl<Str, Par, Col, Out> ParserOnce<Str> for RepeatExact<Par, Col>
 where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: ResultConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Out;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let mut collector = std::mem::take(&mut self.collector);
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
         for _ in 0..self.mode.0 {
-            collector.push(self.parse_stream_oncer.parse_stream_once(input)?);
+            self.collector
+                .extend([result!(self.parser.parse_stream_mut(input))])
         }
-        Ok(collector)
+        Out::ok(self.collector)
     }
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, Minimum, Col>
+impl<Str, Par, Col, Out> ParserOnce<Str> for RepeatMin<Par, Col>
 where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: Recoverable + UnerringConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Out;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let parse_stream_oncer = &mut self.parse_stream_oncer;
-
-        let collector = parse_stream_oncer
-            .as_ref()
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
+        let mut collector = result!((&mut self.parser)
             .repeat_exact(self.mode.0)
-            .collector(std::mem::take(&mut self.collector))
-            .parse_stream_once(input)?;
-
-        let collector = parse_stream_oncer
-            .as_ref()
-            .repeat()
-            .collector(collector)
-            .parse_stream_once(input)
-            .unwrap();
-
-        Ok(collector)
+            .collector(self.collector)
+            .parse_stream_once(input));
+        Out::ok(
+            self.parser
+                .repeat()
+                .collector(collector)
+                .parse_stream_once(input)
+                .unwrap(),
+        )
     }
 }
 
-impl<Par, Inp, Col> ParserOnce for Repeater<Par, MinimumEOI, Col>
+impl<Str, Par, Col, Out> ParserOnce<Str> for RepeatMinEOI<Par, Col>
 where
-    Par: ParserOnce,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Col: Extend<Out::Value>,
+    Out: UnerringConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Out;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let parse_stream_oncer = &mut self.parse_stream_oncer;
-
-        let collector = parse_stream_oncer
-            .as_ref()
+    fn parse_stream_once(mut self, input: &mut Str) -> Self::Output {
+        let mut collector = result!((&mut self.parser)
             .repeat_exact(self.mode.0)
-            .collector(std::mem::take(&mut self.collector))
-            .parse_stream_once(input)?;
-
-        let collector = parse_stream_oncer
-            .as_ref()
+            .collector(self.collector)
+            .parse_stream_once(input));
+        self.parser
             .repeat_eoi()
             .collector(collector)
-            .parse_stream_once(input)?;
-
-        Ok(collector)
+            .parse_stream_once(input)
     }
 }
 
 // Interspersed
 
-impl<Par, Inp, Int, Col> ParserOnce for Repeater<Par, Inter<UntilErr, Int>, Col>
+/*impl<Str, Par, Int, Col, Out> ParserOnce<Str> for Repeater<Par, Inter<UntilErr, Int>, Col>
 where
-    Par: ParserOnce,
-    Int: ParserOnce<Inp, Error = Par::Error>,
-    Col: Collector<Par::Output>,
+    Str: Stream,
+    Par: ParserMut<Str, Output = Out>,
+    Int: ParserMut<Str>,
+    Col: Extend<Out::Value>,
+    Out: Recoverable + Combinable<Int::Output> + UnerringConvertable<Value = Col>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Infallible;
+    type Output = Out;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
-        let mut collector = std::mem::take(&mut self.collector);
-        let Inter(_, ref mut int) = self.mode;
+    fn parse_stream_once(self, input: &mut Str) -> Self::Output {
+        let Inter(UntilErr, ref mut int) = self.mode;
 
         match self
-            .parse_stream_oncer
-            .as_ref()
+            .parser
             .non_terminal()
-            .parse_stream_once(input)
+            .parse_stream_mut(input)
+            .into_result()
         {
-            Ok(value) => collector.push(value),
-            Err(_) => return Ok(collector),
+            Ok(value) => self.collector.extend([value]),
+            Err(_) => return Out::ok(self.collector),
         }
 
-        int.as_ref()
-            .then(self.parse_stream_oncer.as_ref())
+        int.and(self.parser)
             .repeat()
-            .collector(collector)
+            .collector(self.collector)
             .parse_stream_once(input)
     }
 }
 
-impl<Par, Inp, Int, Col> ParserOnce for Repeater<Par, Inter<Maximum, Int>, Col>
+impl<Str, Par, Int, Col> ParserOnce<Str> for Repeater<Par, Inter<Maximum, Int>, Col>
 where
-    Par: ParserOnce,
-    Int: ParserOnce<Inp, Error = Par::Error>,
+    Str: Stream,
+    Par: ParserOnce<Str>,
+    Int: ParserOnce<Str>,
     Col: Collector<Par::Output>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Infallible;
+    type Output = Pure<Col>;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
+    fn parse_stream_once(self, input: &mut Str) -> Self::Output {
         let mut collector = std::mem::take(&mut self.collector);
         let Inter(Maximum(maximum), ref mut int) = self.mode;
 
@@ -312,36 +312,31 @@ where
             return Ok(collector);
         }
 
-        match self
-            .parse_stream_oncer
-            .as_ref()
-            .non_terminal()
-            .parse_stream_once(input)
-        {
+        match self.parser.non_terminal().parse_stream_once(input) {
             Ok(value) => collector.push(value),
             Err(_) => return Ok(collector),
         }
 
-        int.as_ref()
-            .then(self.parse_stream_oncer.as_ref())
+        int.then(self.parser)
             .repeat_max(maximum - 1)
             .collector(collector)
             .parse_stream_once(input)
     }
 }
 
-impl<Par, Inp, Int, Col> ParserOnce for Repeater<Par, Inter<Exact, Int>, Col>
+impl<Str, Par, Int, Col> ParserOnce<Str> for Repeater<Par, Inter<Exact, Int>, Col>
 where
-    Par: ParserOnce,
-    Int: ParserOnce<Inp, Error = Par::Error>,
+    Str: Stream,
+    Par: ParserOnce<Str>,
+    Int: ParserOnce<Str>,
+    Par::Output: Exceptional,
+    Int::Output: Exceptional<Error = <Par::Output as Exceptional>::Error>,
     Col: Collector<Par::Output>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Result<Col, err![Par]>;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
+    fn parse_stream_once(self, input: &mut Str) -> Self::Output {
         let mut collector = std::mem::take(&mut self.collector);
         let Inter(Exact(exact), ref mut int) = self.mode;
 
@@ -349,42 +344,40 @@ where
             return Ok(collector);
         }
 
-        collector.push(self.parse_stream_oncer.parse_stream_once(input)?);
+        collector.push(self.parser.parse_stream_once(input)?);
 
-        int.as_ref()
-            .then(self.parse_stream_oncer.as_ref())
+        int.then(self.parser)
             .repeat_exact(exact - 1)
             .collector(collector)
             .parse_stream_once(input)
     }
 }
 
-impl<Par, Inp, Int, Col> ParserOnce for Repeater<Par, Inter<Minimum, Int>, Col>
+impl<Str, Par, Int, Col> ParserOnce<Str> for Repeater<Par, Inter<Minimum, Int>, Col>
 where
-    Par: ParserOnce,
-    Int: ParserOnce<Inp, Error = Par::Error>,
+    Str: Stream,
+    Par: ParserOnce<Str>,
+    Int: ParserOnce<Str>,
+    Par::Output: Exceptional,
+    Int::Output: Exceptional<Error = <Par::Output as Exceptional>::Error>,
     Col: Collector<Par::Output>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Result<Col, err![Par]>;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
+    fn parse_stream_once(self, input: &mut Str) -> Self::Output {
         let collector = std::mem::take(&mut self.collector);
         let Inter(Minimum(minimum), ref mut int) = self.mode;
 
         let collector = self
-            .parse_stream_oncer
-            .as_ref()
+            .parser
             .repeat_exact(minimum)
             .collector(collector)
-            .interspersed(int.as_ref())
+            .interspersed(int)
             .parse_stream_once(input)?;
 
         Ok(int
-            .as_ref()
-            .then(self.parse_stream_oncer.as_ref())
+            .then(self.parser)
             .repeat()
             .collector(collector)
             .parse_stream_once(input)
@@ -392,100 +385,118 @@ where
     }
 }
 
-impl<Par, Inp, Int, Col> ParserOnce for Repeater<Par, Inter<MinimumEOI, Int>, Col>
+impl<Str, Par, Int, Col> ParserOnce<Str> for Repeater<Par, Inter<MinimumEOI, Int>, Col>
 where
-    Par: ParserOnce,
-    Int: ParserOnce<Inp, Error = Par::Error>,
+    Str: Stream,
+    Par: ParserOnce<Str>,
+    Int: ParserOnce<Str>,
+    Par::Output: Exceptional,
+    Int::Output: Exceptional<Error = <Par::Output as Exceptional>::Error>,
     Col: Collector<Par::Output>,
 {
-    type Input = Par::Input;
-    type Output = Col;
-    type Error = Par::Error;
+    type Output = Result<Col, err![Par]>;
 
     #[inline]
-    fn parse_stream_once(&mut self, input: &mut Self::Input) -> Self::Output {
+    fn parse_stream_once(self, input: &mut Str) -> Self::Output {
         let collector = std::mem::take(&mut self.collector);
         let Inter(MinimumEOI(minimum), ref mut int) = self.mode;
 
         let collector = self
-            .parse_stream_oncer
-            .as_ref()
+            .parser
             .repeat_exact(minimum)
             .collector(collector)
-            .interspersed(int.as_ref())
+            .interspersed(int)
             .parse_stream_once(input)?;
 
         Ok(int
-            .as_ref()
-            .then(self.parse_stream_oncer.as_ref())
+            .then(self.parser)
             .repeat_eoi()
             .collector(collector)
             .parse_stream_once(input)?)
     }
-}
+}*/
 
 impl<Par, Mod, Col> Repeater<Par, Mod, Col> {
-    #[inline(always)]
-    pub fn interspersed<Int>(self, parse_stream_oncer: Int) -> Repeater<Par, Inter<Mod, Int>, Col> {
+    /*#[inline(always)]
+    pub fn interspersed<Str>(self, parser: Int) -> Repeater<Par, Inter<Mod, Int>, Col> {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
-            mode: Inter(self.mode, parse_stream_oncer),
+            parser: self.parser,
+            mode: Inter(self.mode, parser),
             collector: self.collector,
         }
-    }
+    }*/
 
     #[inline(always)]
-    pub fn collector<T>(self, collector: T) -> Repeater<Par, Mod, T> {
+    pub fn collector<T, Str>(self, collector: T) -> Repeater<Par, Mod, T>
+    where
+        Str: Stream,
+        Par: ParserMut<Str>,
+    {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
+            parser: self.parser,
             mode: self.mode,
             collector,
         }
     }
 
     #[inline(always)]
-    pub fn collect<T>(self) -> Repeater<Par, Mod, T>
+    pub fn collect<T, Str>(self) -> Repeater<Par, Mod, T>
     where
         T: Default,
+        Str: Stream,
+        Par: ParserMut<Str>,
+        Par::Output: Data,
     {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
+            parser: self.parser,
             mode: self.mode,
             collector: T::default(),
         }
     }
 
     #[inline(always)]
-    pub fn to_vec<Inp>(self) -> Repeater<Par, Mod, Vec<Par::Output>>
+    pub fn to_vec<Str>(self) -> Repeater<Par, Mod, Vec<Par::Output>>
     where
-        Par: ParserOnce,
+        Str: Stream,
+        Par: ParserMut<Str>,
+        Par::Output: Data,
     {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
+            parser: self.parser,
             mode: self.mode,
             collector: vec![],
         }
     }
 }
 
-impl<Par, Col> Repeater<Par, UntilErr, Col> {
+impl<Par, Col> Repeat<Par, Col> {
     #[inline(always)]
-    pub fn eoi(self) -> Repeater<Par, UntilEOI, Col> {
+    pub fn until_eoi(self) -> RepeatEOI<Par, Col> {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
+            parser: self.parser,
             mode: UntilEOI,
             collector: self.collector,
         }
     }
 }
 
-impl<Par, Col> Repeater<Par, Minimum, Col> {
+impl<Par, Col> RepeatMin<Par, Col> {
     #[inline(always)]
-    pub fn eoi(self) -> Repeater<Par, MinimumEOI, Col> {
+    pub fn until_eoi(self) -> RepeatMinEOI<Par, Col> {
         Repeater {
-            parse_stream_oncer: self.parse_stream_oncer,
+            parser: self.parser,
             mode: MinimumEOI(self.mode.0),
             collector: self.collector,
         }
     }
 }
+
+macro_rules! result {
+    ($expr:expr) => {
+        match $expr.into_result() {
+            Ok(it) => it,
+            Err(err) => return Out::err(err),
+        }
+    };
+}
+use result;
