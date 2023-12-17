6fdc13b9fe0dd07d06437e46e7d89d2b8f68da3d
Sabi <sabivtuber@proton.me>
2023-12-17 08:08:37 -0300

    Rename "FlatMap" to "TryMap"

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
 #![allow(unused)]
 
+pub mod data {
+    pub mod adapters {
+        pub mod effect;
+        pub mod option;
+        pub mod result;
+        pub mod sure;
+        pub mod unit;
+    }
+    pub(crate) mod prelude;
+    pub mod traits;
+}
 pub mod parser {
     pub mod adapters {
         pub mod and;
-        //pub mod repeat;
+        pub mod repeat;
         //pub mod conversion;
         pub mod attach;
-        pub mod flat_map;
         pub mod ignore;
         pub mod map;
         pub mod map_err;
         pub mod non_terminal;
         pub mod opt;
         pub mod or;
+        pub mod try_map;
     }
     pub(crate) mod prelude;
     pub mod sources;
     pub mod traits;
     pub(crate) mod util;
-}
-pub mod data {
-    pub mod adapters {
-        pub mod effect;
-        pub mod option;
-        pub mod pure;
-        pub mod result;
-        pub mod unit;
-    }
-    pub(crate) mod prelude;
-    pub mod traits;
 }
 pub mod stream {
     pub mod adapters;
--- src/parser/adapters/try_map.rs
+++ src/parser/adapters/try_map.rs
@@ -3,21 +3,21 @@ use crate::parser::prelude::*;
 use crate::stream::traits::Stream;
 use std::marker::PhantomData;
 
-pub struct FlatMap<Par, Fun, Val> {
+pub struct TryMap<Par, Fun, Val> {
     parser: Par,
     function: Fun,
     _marker: PhantomData<Val>,
 }
 
-impl<Par, Fun, Val> FlatMap<Par, Fun, Val> {
-    pub(crate) fn new<Str>(parser: Par, function: Fun) -> FlatMap<Par, Fun, Val>
+impl<Par, Fun, Val> TryMap<Par, Fun, Val> {
+    pub(crate) fn new<Str>(parser: Par, function: Fun) -> TryMap<Par, Fun, Val>
     where
         Str: Stream,
         Par: ParserOnce<Str>,
         Par::Output: Data + Exceptional,
         Fun: Fn(val![Par]) -> val![Par<Val>],
     {
-        FlatMap {
+        TryMap {
             parser,
             function,
             _marker: PhantomData,
@@ -25,7 +25,7 @@ impl<Par, Fun, Val> FlatMap<Par, Fun, Val> {
     }
 }
 
-impl<Str, Par, Fun, Val> ParserOnce<Str> for FlatMap<Par, Fun, Val>
+impl<Str, Par, Fun, Val> ParserOnce<Str> for TryMap<Par, Fun, Val>
 where
     Str: Stream,
     Par: ParserOnce<Str>,
